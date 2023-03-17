8c99ecb7edfe3733b80284c623743e39b7cedb89
azizbek-tursunov <azizbektursunovofficial@gmail.com>
2023-03-17 08:57:18 +0500

    Rolega qarab view o'zgartirildi

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,5 +20,12 @@ class UserSeeder extends Seeder
             'email' => 'manager@company.com',
             'password' => Hash::make('secret')
         ]);
+
+        User::create([
+            'name' => 'Client',
+            'role_id' => 2,
+            'email' => 'client@company.com',
+            'password' => Hash::make('secret')
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (auth()->user()->role->name == 'manager')
+                        {{ __("You're logged in as a manager!") }}
+                    @else
+                        {{ __("You're logged in as an client!") }}
+                    @endif
                 </div>
             </div>
         </div>
